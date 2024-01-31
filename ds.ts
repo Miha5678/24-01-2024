d166a1bbba5d55225627b0c00d8dd62e85bb9415
Кириченко Михаил <mister_miha5678@stud.vvsu.ru>
2024-01-31 18:23:17 +1000

    d

---
+++ ds.ts
@@ -0,0 +1,11 @@
+class ClassName{}
+class Point {x: number; y: number;}
+let point: Point = new Point();
+point.x = 1;
+function pointw(){point.x; point.y;}
+
+class Point2 {width: number; height: number;}
+let point2: Point2 = new Point2();
+point2.width = 1; 
+point2.height = 2;
+function pointw2(){point.x; point.y;}
